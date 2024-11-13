bf404c9d13c1ba985d7e90b04fc1b7384c4aeff6
526cloiacono <526cloiacono@frhsd.com>
2024-11-13 09:28:35 -0500

    Got the first 5 blanks done

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let userName = prompt('What is your name?')
-let userAge = prompt('How old are you')
-console.log(userName)
-console.log(userAge)
+let firstBlank = prompt("Enter a Adj");
+let secondBlank = prompt("Enter a crazy animal");
+let thridBlank = prompt("Enter a silly verb");
+let fourthBlank = prompt("Name a random object");
+let fithBlank = prompt("verb ending in -ing");
+let sixthBlank = prompt("-");
+let seventhBlank = prompt("-");
+let eighthBlank = prompt("-");
+let ninethBlank = prompt("-");
+console.log(firstBlank);
+console.log(secondBlank);
+console.log(thridBlank);
+console.log(fourthBlank);
+console.log(fithBlank);
+console.log(sixthBlank);
+console.log(seventhBlank);
+console.log(eighthBlank);
+console.log(ninethBlank);
 
-let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge} years old?`
-console.log(myStory)
+let myStory = `On Christmas Eve in a ${firstBlank} village, a ${secondBlank} was busy ${thridBlank} presents. Suddenly, it stumbled upon a ${fourthBlank} that was ${fithBlank} in the snow. The (animal) couldn’t believe its eyes! Just then, a (wacky adjective) elf appeared and shouted, “You’ve found Santa’s (absurd noun)!” The (animal) was (hilarious emotion) but asked, “What do I do with this (random noun)?” The elf replied, “You must (silly verb) it to the (even sillier place)!” So, the (animal) set off on a (funny adjective) adventure. Along the way, it met a (funny adjective) snowman who helped it (silly verb) through a (silly obstacle). In the end, they delivered the (absurd noun) just in time for Christmas, and everyone celebrated by (silly verb ending in -ing) under the (silly noun).`;
+console.log(myStory);
 
 document.getElementById('story').innerHTML = myStory;
