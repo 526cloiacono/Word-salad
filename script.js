0c7aa55ef1e9fd57bb1c9566ee1919c2ea059e68
526cloiacono <526cloiacono@frhsd.com>
2024-11-22 09:30:19 -0500

    Did all css and added iframe

--- script.js
+++ script.js
@@ -3,21 +3,11 @@ let animal = prompt("Enter a crazy animal");
 let verb = prompt("Enter a silly verb");
 let obj = prompt("Name a random object");
 let noun = prompt("Name an absurd noun");
-let emotion = prompt("Name an hilarious emotion");
+let emotion = prompt("Name a hilarious emotion");
 let place = prompt("Name an silly place");
 let obstacle = prompt("Name an silly obstacle");
 let verbing = prompt("Enter a silly verb ending in ing");
-console.log(adj);
-console.log(animal);
-console.log(verb);
-console.log(obj);
-console.log(noun);
-console.log(emotion);
-console.log(place);
-console.log(obstacle);
-console.log(verbing);
 
-let myStory = `On Christmas Eve in a ${adj} village, a ${animal} was busy ${verb} presents. Suddenly, it stumbled upon a ${obj} that was ${verb} in the snow. The ${animal} couldn’t believe its eyes! Just then, a ${adj} elf appeared and shouted, “You’ve found Santa’s ${noun}!” The ${animal} was ${emotion} but asked, “What do I do with this ${noun}?” The elf replied, “You must ${verb} it to the ${place}!” So, the ${animal} set off on a ${adj} adventure. Along the way, it met a ${adj} snowman who helped it ${verb} through a ${obstacle}. In the end, they delivered the ${noun} just in time for Christmas, and everyone celebrated by ${verbing} under the ${noun}.`;
-console.log(myStory);
+let myStory = `On Christmas Eve in a <span class="adj">${adj}</span> village, a <span class="animal">${animal}</span> was busy <span class="verb">${verb}</span> presents. Suddenly, it stumbled upon a <span class="obj">${obj}</span> that was <span class="verb">${verb}</span> in the snow. The <span class="animal">${animal}</span> couldn’t believe its eyes! Just then, a <span class="adj">${adj}</span> elf appeared and shouted, “You’ve found Santa’s <span class="noun">${noun}</span>!” The <span class="animal">${animal}</span> was <span class="emotion">${emotion}</span> but asked, “What do I do with this <span class="noun">${noun}</span>?” The elf replied, “You must <span class="verb">${verb}</span> it to the <span class="place">${place}</span>!” So, the <span class="animal">${animal}</span> set off on a <span class="adj">${adj}</span> adventure. Along the way, it met a <span class="adj">${adj}</span> snowman who helped it <span class="verb">${verb}</span> through a <span class="obstacle">${obstacle}</span>. In the end, they delivered the <span class="noun">${noun}</span> just in time for Christmas, and everyone celebrated by <span class="verbing">${verbing}</span> under the <span class="noun">${noun}</span>.`;
 
 document.getElementById('story').innerHTML = myStory;
